af47116f5a0e41fd0fbe6206f22aed780a82f05a
Nakyeoungmi <qwerty5506a@naver.com>
2025-09-29 11:15:37 +0900

    Update oneshot.v

--- oneshot.v
+++ oneshot.v
@@ -10,14 +10,8 @@ module oneshot(
     if (!rst) begin
       btn_d <= 1'b0;
       trig  <= 1'b0;
-    end 
-    else begin
-      btn_d <= btn;
-      // 버튼이 0→1로 바뀌는 순간에만 trig = 1
-      if (btn & ~btn_d)
-        trig <= 1'b1;
-      else
-        trig <= 1'b0;
+    end else begin 
+      btn_d <= btn; trig <= btn & ~btn_d; 
     end
   end
 endmodule
