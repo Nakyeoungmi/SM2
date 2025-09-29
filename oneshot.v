8b438603039a63cc4d4d4972e073d78c7329e72c
Nakyeoungmi <qwerty5506a@naver.com>
2025-09-29 09:29:10 +0900

    Update oneshot.v

--- oneshot.v
+++ oneshot.v
@@ -1,3 +1,4 @@
+`timescale 1ns / 1ps
 module oneshot(
   input clk, rst, btn,
   output reg trig
@@ -5,12 +6,18 @@ module oneshot(
 
   reg btn_d;
 
-  always @(posedge clk or negedge rst)begin
+  always @(posedge clk) begin
     if (!rst) begin
       btn_d <= 1'b0;
-      trig <= 1'b0;
-    end else begin
+      trig  <= 1'b0;
+    end 
+    else begin
       btn_d <= btn;
-      trig <= btn & ~btn_d;
+      // 버튼이 0→1로 바뀌는 순간에만 trig = 1
+      if (btn & ~btn_d)
+        trig <= 1'b1;
+      else
+        trig <= 1'b0;
     end
   end
+endmodule
